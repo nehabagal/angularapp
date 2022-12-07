e1b48df0556c96f294abee646c06d68ac01363b8
nehabagal <nehabagal171@gmail.com>
2022-12-07 11:22:36 +0530

    Template reference variable

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -7,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
   userName:string = 'Codemind';
+  uname;
   getUserName(){
     this.userName='';
   }
-
+show(value){
+this.uname=value;
+  console.log(value);
+  
+}
   constructor() { }
 
   ngOnInit() {
